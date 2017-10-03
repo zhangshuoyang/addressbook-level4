4bc01b52cd6c7e6ba5d8a1990317b0b540edfe96
Zhang Shuoyang <zhangshuoyang@Zhangs-MacBook-Pro-3.local>
2017-10-04 00:09:13 +0800

    UI: added comments

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -11,10 +11,13 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.ReadOnlyPerson;
 
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
-    private static String[] colors = {"red", "blue", "yellow", "orange", "brown", "green", "pink", "black", "grey"};
+    private static String[] colors = {"red", "yellow", "blue", "orange", "brown", "green", "pink", "black", "grey"};
     private static HashMap<String, String> tagColors = new HashMap<String, String>();
     private static Random random = new Random();
 
@@ -75,6 +78,9 @@ public class PersonCard extends UiPart<Region> {
         });
     }
 
+    /**
+     * Dummy comment
+     */
     private void initTags(ReadOnlyPerson person) {
         person.getTags().forEach(tag -> {
             Label tagLabel = new Label(tag.tagName);
