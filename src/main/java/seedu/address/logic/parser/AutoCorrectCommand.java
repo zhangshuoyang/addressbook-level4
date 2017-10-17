15454f136939e87d69d57d8c2ec0ee773a6d9d37
JYL123 <li474412210@gmail.com>
2017-10-17 17:03:47 +0800

    Implement getCommandPool method

--- src/main/java/seedu/address/logic/parser/AutoCorrectCommand.java
+++ src/main/java/seedu/address/logic/parser/AutoCorrectCommand.java
@@ -2,6 +2,22 @@ package seedu.address.logic.parser;
 
 import java.util.ArrayList;
 
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteTagCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.MultiFilterCommand;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.SearchCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.UndoCommand;
+
 public class AutoCorrectCommand {
 
     public static ArrayList<String> editDistance1(String word) {
@@ -55,59 +71,77 @@ public class AutoCorrectCommand {
     public static String correctWord (String word) {
 
         word = word.toLowerCase();
-        ArrayList<String> commandPool = new ArrayList<String>();
-        commandPool.add("find");
-        commandPool.add("add");
-        commandPool.add("search");
-        commandPool.add("delete");
+        ArrayList<String> commandPool = getCommandPool();
 
         if(commandPool.contains(word)) {
             return word;
         }
 
-        ArrayList<String> editDistance1WordsAdd = editDistance1("add");
+        ArrayList<String> editDistance1WordsAdd = editDistance1(AddCommand.COMMAND_WORD);
         ArrayList<ArrayList<String>> editDistance2WordsAdd = new ArrayList<ArrayList<String>>();
         for (String editDistance1WordAdd : editDistance1WordsAdd) {
             editDistance2WordsAdd.add(editDistance1(editDistance1WordAdd));
         }
 
         if(editDistance1WordsAdd.contains(word)) {
-            return "add";
+            return AddCommand.COMMAND_WORD;
         }
 
         for(ArrayList<String> editDistance2WordAdd : editDistance2WordsAdd) {
-            if(editDistance2WordAdd.contains(word)) return "add";
+            if(editDistance2WordAdd.contains(word)) return AddCommand.COMMAND_WORD;
         }
 
-        ArrayList<String> editDistance1WordsDelete = editDistance1("delete");
+        ArrayList<String> editDistance1WordsDelete = editDistance1(DeleteCommand.COMMAND_WORD);
         ArrayList<ArrayList<String>> editDistance2WordsDelete = new ArrayList<ArrayList<String>>();
         for (String editDistance1WordDelete : editDistance1WordsDelete) {
             editDistance2WordsDelete.add(editDistance1(editDistance1WordDelete));
         }
 
         if(editDistance1WordsDelete.contains(word)) {
-            return "delete";
+            return DeleteCommand.COMMAND_WORD;
         }
 
         for(ArrayList<String> editDistance2WordDelete : editDistance2WordsDelete) {
-            if(editDistance2WordDelete.contains(word)) return "delete";
+            if(editDistance2WordDelete.contains(word)) return DeleteCommand.COMMAND_WORD;
         }
 
-        ArrayList<String> editDistance1WordsSearch = editDistance1("search");
+        ArrayList<String> editDistance1WordsSearch = editDistance1(SearchCommand.COMMAND_WORD);
         ArrayList<ArrayList<String>> editDistance2WordsSearch = new ArrayList<ArrayList<String>>();
         for (String editDistance1WordSearch : editDistance1WordsSearch) {
             editDistance2WordsSearch.add(editDistance1(editDistance1WordSearch));
         }
 
         if(editDistance1WordsSearch.contains(word)) {
-            return "search";
+            return SearchCommand.COMMAND_WORD;
         }
 
         for(ArrayList<String> editDistance2WordSearch : editDistance2WordsSearch) {
-            if(editDistance2WordSearch.contains(word)) return "search";
+            if(editDistance2WordSearch.contains(word)) return SearchCommand.COMMAND_WORD;
         }
 
         return "no such command";
+    }
+
+    public static ArrayList<String> getCommandPool () {
+
+        ArrayList<String> commandPool = new ArrayList<String>();
+        commandPool.add(FindCommand.COMMAND_WORD);
+        commandPool.add(AddCommand.COMMAND_WORD);
+        commandPool.add(SearchCommand.COMMAND_WORD);
+        commandPool.add(DeleteCommand.COMMAND_WORD);
+        commandPool.add(DeleteTagCommand.COMMAND_WORD);
+        commandPool.add(ClearCommand.COMMAND_WORD);
+        commandPool.add(EditCommand.COMMAND_WORD);
+        commandPool.add(ExitCommand.COMMAND_WORD);
+        commandPool.add(HelpCommand.COMMAND_WORD);
+        commandPool.add(HistoryCommand.COMMAND_WORD);
+        commandPool.add(ListCommand.COMMAND_WORD);
+        commandPool.add(MultiFilterCommand.COMMAND_WORD);
+        commandPool.add(RedoCommand.COMMAND_WORD);
+        commandPool.add(SelectCommand.COMMAND_WORD);
+        commandPool.add(UndoCommand.COMMAND_WORD);
+
+        return commandPool;
     }
 
 }
