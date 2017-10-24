7ca7ec4e3160938add788b3852eedf7101a17491
DESKTOP-OOCVVM6\Surface <chairuizhong93@hotmail.com>
2017-10-24 16:25:49 +0800

    UI design to display task

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,17 +1,25 @@
 package seedu.address.ui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.NewResultAvailableEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -29,18 +37,32 @@ public class BrowserPanel extends UiPart<Region> {
 
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
+    private final StringProperty displayed = new SimpleStringProperty("");
+
     @FXML
     private WebView browser;
+
+    @FXML
+    private TextArea taskDisplayed;
+
 
     public BrowserPanel() {
         super(FXML);
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
-
+        displayTask();
         loadDefaultPage();
+        displayOnTextArea();
         registerAsAnEventHandler(this);
     }
+
+    private void displayTask() {
+        //taskDisplayed.textProperty().bind(displayed);
+        taskDisplayed.setStyle("-fx-font-family: monospace; -fx-background-color: #f8ecc2; -fx-font-size: 22px;");
+
+    }
+
 
     private void loadPersonPage(ReadOnlyPerson person) {
         loadPage(GOOGLE_MAPS_SEARCH_URL_PREFIX + person.getAddress().toString()
@@ -66,9 +88,34 @@ public class BrowserPanel extends UiPart<Region> {
         browser = null;
     }
 
+    /**
+     *  Read and Display the task on the Text Area
+     */
+    public void displayOnTextArea() {
+        try {
+            String curr = System.getProperty("user.dir");
+            Scanner s = new Scanner(new File(curr + "/data/Hello.txt")).useDelimiter
+                    (Pattern.compile("\\r\\n"));
+            while (s.hasNext()) {
+
+                taskDisplayed.appendText(s.next() + "\n");
+
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+        }
+
+    }
+
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         loadPersonPage(event.getNewSelection().person);
+    }
+
+    @Subscribe
+    private void handleNewTaskAvailableEvent(NewResultAvailableEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        //Platform.runLater(() -> displayed.setValue());
     }
 }
