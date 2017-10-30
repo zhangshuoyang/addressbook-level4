af80c8cad94fe61b10a41720de43e9df07eea02f
Zhang Shuoyang <zhangshuoyang@Zhangs-MacBook-Pro-3.local>
2017-10-30 14:07:54 +0800

    Logic: change prefix of add task command

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,8 +13,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
-    public static final Prefix PREFIX_DESCIPTION = new Prefix("d/");
-    public static final Prefix PREFIX_PRIORITY = new Prefix("priority/");
-    public static final Prefix PREFIX_DUEDATE = new Prefix("on/");
+    public static final Prefix PREFIX_DESCIPTION = new Prefix("*/");
+    public static final Prefix PREFIX_PRIORITY = new Prefix("pr/");
+    public static final Prefix PREFIX_DUEDATE = new Prefix("on ");
 
 }
