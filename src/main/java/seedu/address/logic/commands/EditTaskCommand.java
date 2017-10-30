f985320e154387aef5c83e5f48c0fb48c7e34cf1
JYL123 <li474412210@gmail.com>
2017-10-30 23:34:11 +0800

    Create editTaskCommand class

---
+++ src/main/java/seedu/address/logic/commands/EditTaskCommand.java
@@ -0,0 +1,194 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.AutoCorrectCommand;
+import seedu.address.model.task.Description;
+import seedu.address.model.task.DueDate;
+import seedu.address.model.task.Priority;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.exceptions.DuplicateTaskException;
+import seedu.address.model.task.exceptions.TaskNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.commands.AddTaskCommand.MESSAGE_DUPLICATE_TASK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DUEDATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
+
+public class EditTaskCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "edit task";
+    public static final String COMMAND_WORD_ALIAS = "e task";
+    public static final String AUTOCOMPLETE_FORMAT =
+            COMMAND_WORD + " "
+                    + PREFIX_DESCIPTION + "task "
+                    + PREFIX_PRIORITY + "priority (2, 1, 0) "
+                    + PREFIX_DUEDATE + "date (dd/MM/yyyy) ";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the task identified "
+            + "by the index number used in the last task listing. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_DESCIPTION + "task] "
+            + "[" + PREFIX_PRIORITY + "priority (2, 1, 0)] "
+            + "[" + PREFIX_DUEDATE + "date (dd/MM/yyyy) ";
+
+    public static final String MESSAGE_EDIT_TASK_SUCCESS = "Edited task: %1$s";
+
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task book.";
+
+    private final Index index;
+    private final EditTaskDescriptor editTaskDescriptor;
+
+    private AutoCorrectCommand autoCorrectCommand = new AutoCorrectCommand();
+
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param editTaskDescriptor details to edit the person with
+     */
+    public EditTaskCommand(Index index, EditTaskDescriptor editTaskDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editTaskDescriptor);
+
+        this.index = index;
+        this.editTaskDescriptor = new EditTaskDescriptor(editTaskDescriptor);
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        List<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToEdit = lastShownList.get(index.getZeroBased());
+        Task editedTask = createEditedTask(taskToEdit, editTaskDescriptor);
+
+        try {
+            model.updateTask(taskToEdit, editedTask);
+        } catch (DuplicateTaskException dte) {
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
+        } catch (TaskNotFoundException tnfe) {
+            throw new AssertionError("The target task cannot be missing");
+        }
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+
+        if (autoCorrectCommand.getMessageToUser().equals("")) {
+            return new CommandResult(String.format("MESSAGE_EDIT_TASK_SUCCESS", editedTask));
+        } else {
+            return new CommandResult(autoCorrectCommand.getMessageToUser()
+                    + "\n"
+                    + String.format("MESSAGE_EDIT_TASK_SUCCESS", editedTask));
+        }
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Task createEditedTask(ReadOnlyTask taskToEdit,
+                                             EditTaskDescriptor editTaskDescriptor) {
+        assert taskToEdit != null;
+
+        Description updatedDescription = editTaskDescriptor.getDescription().orElse(taskToEdit.getDescription());
+        Priority updatedPriority = editTaskDescriptor.getPriority().orElse(taskToEdit.getPriority());
+        DueDate updateDueDate = editTaskDescriptor.getDueDate().orElse(taskToEdit.getDueDate());
+
+        return new Task(updatedDescription, updatedPriority, updateDueDate);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditTaskCommand)) {
+            return false;
+        }
+
+        // state check
+        EditTaskCommand e = (EditTaskCommand) other;
+        return index.equals(e.index)
+                && editTaskDescriptor.equals(e.editTaskDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditTaskDescriptor {
+        private Description description;
+        private Priority priority;
+        private DueDate dueDate;
+
+        public EditTaskDescriptor() {}
+
+        public EditTaskDescriptor(EditTaskDescriptor toCopy) {
+            this.description = toCopy.description;
+            this.priority = toCopy.priority;
+            this.dueDate = toCopy.dueDate;
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(this.description, this.priority, this.dueDate);
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+
+        public Optional<Description> getDescription() {
+            return Optional.ofNullable(description);
+        }
+
+        public void setPriority(Priority priority) {
+            this.priority = priority; }
+
+        public Optional<Priority> getPriority() {
+            return Optional.ofNullable(priority);
+        }
+
+        public void setDueDate(DueDate dueDate) {
+            this.dueDate = dueDate; }
+
+        public Optional<DueDate> getDueDate() {
+            return Optional.ofNullable(dueDate);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditTaskDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditTaskDescriptor e = (EditTaskDescriptor) other;
+
+            return getDescription().equals(e.getDescription())
+                    && getPriority().equals(e.getPriority())
+                    && getDueDate().equals(e.getDueDate());
+        }
+    }
+
+}
