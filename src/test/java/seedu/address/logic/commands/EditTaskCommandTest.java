151072119482a22ac23a4e2d27dd24f028fcd828
JYL123 <li474412210@gmail.com>
2017-10-31 22:25:42 +0800

    Add editTaskCommandTest file

--- src/test/java/seedu/address/logic/commands/EditTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditTaskCommandTest.java
@@ -1,35 +1,21 @@
 package seedu.address.logic.commands;
 
+import org.junit.Rule;
 import org.junit.Test;
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.task.Task;
-import seedu.address.testutil.TaskBuilder;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import seedu.address.logic.commands.EditTaskCommand.EditTaskDescriptor;
+import org.junit.rules.ExpectedException;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
  */
 public class EditTaskCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void execute_allFieldsSpecifiedUnfilteredList_success() throws Exception {
-        Task editedTask = new TaskBuilder().build();
-        EditTaskDescriptor descriptor = new EditTaskDescriptorBuilder(editedTask).build();
-        EditTaskCommand editTaskCommand = prepareCommand(INDEX_FIRST_TASK.get(0), descriptor);
-
-        String expectedMessage = String.format(EditTaskCommand.MESSAGE_EDIT_TASK_SUCCESS, editedTask);
-
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModel.updateTask(model.getFilteredTaskList().get(0), editedTask);
-
-        assertCommandSuccess(editTaskCommand, model, expectedMessage, expectedModel);
+    public void constructor_nullPerson_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new EditTaskCommand(null, new EditTaskCommand.EditTaskDescriptor());
     }
 }
+
