da54bc138ed73f8546bb1f28b30fe6095fe62320
JYL123 <li474412210@gmail.com>
2017-10-31 22:25:09 +0800

    Add more test data

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -3,10 +3,13 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DUEDATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE_2;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.ArrayList;
@@ -57,6 +60,14 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+
+    public static final String VALID_TASK_DESC__AMY = "Task Amy";
+    public static final String VALID_TASK_PRIORITY__AMY = "1";
+    public static final String VALID_TASK_DUEDATE__AMY = "30/12/2017";
+
+    public static final String DESC_TASK_AMY = " " + PREFIX_DESCIPTION + VALID_TASK_DESC__AMY;
+    public static final String PRIORITY_TASK_AMY = " " + PREFIX_PRIORITY + VALID_TASK_PRIORITY__AMY;
+    public static final String DUEDATE_TASK_AMY = " " + PREFIX_DUEDATE + VALID_TASK_DUEDATE__AMY;
 
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
