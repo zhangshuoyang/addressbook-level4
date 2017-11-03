8c4bc0126a880c3c67967985ba8d405448c6dc7e
Zhang Shuoyang <zhangshuoyang@r-174-106-25-172.comp.nus.edu.sg>
2017-11-03 12:46:20 +0800

    checkstyle again

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,9 +1,9 @@
 package seedu.address.ui;
 
-import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
